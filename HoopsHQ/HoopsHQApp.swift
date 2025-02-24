8b34808755c6bf6376a335083baf96003cd38bc5
billyh2 <billyh2@illinois.edu>
2025-02-23 19:45:13 -0600

    Initial Commit

---
+++ HoopsHQ/HoopsHQApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopsHQApp.swift
+//  HoopsHQ
+//
+//  Created by Billy Huang on 2025/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HoopsHQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
